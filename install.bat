9fc2a94e9e6e88f7c7b8f17afd91752815482339
Ben Croughs <bc3@telenet.be>
2021-04-26 19:20:46 +0200

    initial commit

---
+++ install.bat
@@ -0,0 +1,19 @@
+:: Install choco .exe and add choco to PATH
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+:: Install all the packages
+:::: Browsers
+choco install googlechrome -fy
+choco install firefox -fy
+:::: Text editors / IDEs
+choco install visualstudiocode -fy
+choco install visualstudio2019community -fy
+
+:::: Dev tools
+choco install git -fy
+choco install nodejs.install -fy
+choco install microsoft-windows-terminal --pre -fy
+choco install ditto.install -fy
+choco install powershell-core -fy
+choco install git-fork -fy
+choco install resharper-ultimate-all -fy
+choco install notepadplusplus -fy
